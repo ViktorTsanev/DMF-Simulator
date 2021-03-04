94085d376600e43e0548a6af4927f6b6da9e5383
Viktor Anzhelev Tsanev <s184453@dtu.dk>
2021-03-04 23:16:51 +0100

    json data loading started

---
+++ DMF-Simulator-Frontend/Models/BoardModel.cs
@@ -0,0 +1,55 @@
+﻿
+public class BoardModel
+{
+    public Information[] information { get; set; }
+    public Electrode[] electrodes { get; set; }
+    public object actuators { get; set; }
+    public object sensors { get; set; }
+    public Input[] inputs { get; set; }
+    public Output[] outputs { get; set; }
+    public object droplets { get; set; }
+    public object bubbles { get; set; }
+    public object unclassified { get; set; }
+}
+
+public class Information
+{
+    public string platform_name { get; set; }
+    public string platform_type { get; set; }
+    public string platform_ID { get; set; }
+    public int sizeX { get; set; }
+    public int sizeY { get; set; }
+}
+
+public class Electrode
+{
+    public string name { get; set; }
+    public int ID { get; set; }
+    public int electrodeID { get; set; }
+    public int driverID { get; set; }
+    public int shape { get; set; }
+    public int positionX { get; set; }
+    public int positionY { get; set; }
+    public int sizeX { get; set; }
+    public int sizeY { get; set; }
+    public int status { get; set; }
+    public object corners { get; set; }
+}
+
+public class Input
+{
+    public string name { get; set; }
+    public int ID { get; set; }
+    public int inputID { get; set; }
+    public int positionX { get; set; }
+    public int positionY { get; set; }
+}
+
+public class Output
+{
+    public string name { get; set; }
+    public int ID { get; set; }
+    public int outputID { get; set; }
+    public int positionX { get; set; }
+    public int positionY { get; set; }
+}
--- DMF-Simulator-Frontend/Models/ElectrodeModel.cs
+++ DMF-Simulator-Frontend/Models/ElectrodeModel.cs
@@ -4,7 +4,7 @@ namespace DMF_Simulator_Frontend.Models
 {
     public class ElectrodeModel
     {
-        public int DistanceFromBottom { get; private set; } = new Random().Next(0, 500);
-        public int DistanceFromLeft { get; private set; } = new Random().Next(0, 500);
+        public int DistanceFromBottom { get; private set; } = new Random().Next(0, 400);
+        public int DistanceFromLeft { get; private set; } = new Random().Next(0, 400);
     }
 }
