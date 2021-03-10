a031a834f36fde034399277e8eeacd670a616f23
Viktor Anzhelev Tsanev <s184453@dtu.dk>
2021-03-10 22:57:03 +0100

    Cleanup, electrode logic improved

--- DMF-Simulator-Frontend/Models/ElectrodeModel.cs
+++ DMF-Simulator-Frontend/Models/ElectrodeModel.cs
@@ -1,19 +1,17 @@
-﻿using System;
-
-namespace DMF_Simulator_Frontend.Models
+﻿namespace DMF_Simulator_Frontend.Models
 {
     public class ElectrodeModel
     {
-        public string name { get; set; }
+        public string Name { get; set; }
         public int ID { get; set; }
-        public int electrodeID { get; set; }
-        public int driverID { get; set; }
-        public int shape { get; set; }
-        public int positionX { get; set; } //= new Random().Next(0, 400);
-        public int positionY { get; set; } //= new Random().Next(0, 400);
-        public int sizeX { get; set; }
-        public int sizeY { get; set; }
-        public int status { get; set; }
-        public string corners { get; set; }
+        public int ElectrodeID { get; set; }
+        public int DriverID { get; set; }
+        public int Shape { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int SizeX { get; set; }
+        public int SizeY { get; set; }
+        public int Status { get; set; }
+        public string Corners { get; set; }
     }
 }
