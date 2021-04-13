5a5e02c9be0ec67d13cf012405d9d8921f40d7d9
Viktor Anzhelev Tsanev <s184453@dtu.dk>
2021-04-13 12:01:25 +0200

    Toast Notification when uploading files

--- DMF-Simulator-Frontend/Program.cs
+++ DMF-Simulator-Frontend/Program.cs
@@ -1,12 +1,8 @@
 using MatBlazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DMF_Simulator_Frontend
@@ -21,6 +17,16 @@ namespace DMF_Simulator_Frontend
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             
             builder.Services.AddMatBlazor();
+
+            builder.Services.AddMatToaster(config =>
+            {
+                config.Position = MatToastPosition.BottomRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = true;
+                config.ShowCloseButton = true;
+                config.MaximumOpacity = 95;
+                config.VisibleStateDuration = 3000;
+            });
 
             await builder.Build().RunAsync();
         }
