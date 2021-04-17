8b12fd63c98e186781c9391e7e372a0287a0d060
Viktor Anzhelev Tsanev <s184453@dtu.dk>
2021-04-17 13:45:08 +0200

    Support electrode changes Disable visibility toggle for null elements

--- DMF-Simulator-Frontend/Models/SimulatorManager.cs
+++ DMF-Simulator-Frontend/Models/SimulatorManager.cs
@@ -32,19 +32,39 @@ namespace DMF_Simulator_Frontend.Models
         {
             foreach (BoardModel b in BoardModelNew)
             {
+                BoardModel.Electrodes.ForEach(delegate (ElectrodeModel e)
+                {
+                    ElectrodeModel newElectrode = b.Electrodes.Where(t => t.ID == e.ID).FirstOrDefault();
+                    if (newElectrode != null)
+                    {
+                        e.TranslateX += newElectrode.PositionX - e.PositionX - e.TranslateX;
+                        Console.WriteLine("TrX {0} ID {1}", e.TranslateX, e.ID);
+                        e.TranslateY += newElectrode.PositionY - e.PositionY - e.TranslateY;
+                        Console.WriteLine("TrY {0} ID {1}", e.TranslateY, e.ID);
+                        e.ScaleX = (double)newElectrode.SizeX / e.SizeX;
+                        Console.WriteLine("ScaleX {0} ID {1}", e.ScaleX, e.ID);
+                        e.ScaleY = (double)newElectrode.SizeY / e.SizeY;
+                        Console.WriteLine("ScaleY {0} ID {1}", e.ScaleY, e.ID);
+                        e.Status = newElectrode.Status;
+                    }
+                });
                 BoardModel.Droplets.ForEach(delegate (DropletModel d)
                 {
                     DropletModel newDroplet = b.Droplets.Where(t => t.ID == d.ID).FirstOrDefault();
-                    d.TranslateX += newDroplet.PositionX - d.PositionX - d.TranslateX;
-                    Console.WriteLine("TrX {0} ID {1}", d.TranslateX, d.ID);
-                    d.TranslateY += newDroplet.PositionY - d.PositionY - d.TranslateY;
-                    Console.WriteLine("TrY {0} ID {1}", d.TranslateY, d.ID);
-                    d.ScaleX = (double)newDroplet.SizeX / d.SizeX;
-                    Console.WriteLine("ScaleX {0} ID {1}", d.ScaleX, d.ID);
-                    d.ScaleY = (double)newDroplet.SizeY / d.SizeY;
-                    Console.WriteLine("ScaleY {0} ID {1}", d.ScaleY, d.ID);
-                    d.Color = newDroplet.Color;
+                    if (newDroplet != null)
+                    {
+                        d.TranslateX += newDroplet.PositionX - d.PositionX - d.TranslateX;
+                        Console.WriteLine("TrX {0} ID {1}", d.TranslateX, d.ID);
+                        d.TranslateY += newDroplet.PositionY - d.PositionY - d.TranslateY;
+                        Console.WriteLine("TrY {0} ID {1}", d.TranslateY, d.ID);
+                        d.ScaleX = (double)newDroplet.SizeX / d.SizeX;
+                        Console.WriteLine("ScaleX {0} ID {1}", d.ScaleX, d.ID);
+                        d.ScaleY = (double)newDroplet.SizeY / d.SizeY;
+                        Console.WriteLine("ScaleY {0} ID {1}", d.ScaleY, d.ID);
+                        d.Color = newDroplet.Color;
+                    }
                 });
+                
                 MainLoopCompleted?.Invoke(this, EventArgs.Empty);
                 await Task.Delay(1000);
             }
